1dde1e02dad33502c1e72987b90b4b45dc5ef79d
Gabriel Jose de Oliveira <gabriel.oliveira@liveonsolutions.com>
2020-04-30 17:18:56 -0300

    container component

---
+++ src/components/Container/index.js
@@ -0,0 +1,7 @@
+import styled from 'styled-components';
+
+export const Container = styled.View`
+  flex: 1;
+  background: #191920;
+  padding: 15px 20px;
+`;
--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,6 +1,12 @@
 import React from 'react';
 import { Text } from 'react-native';
 
+import { Container } from '../../components/Container';
+
 export default function Home() {
-  return <Text>Home</Text>;
+  return (
+    <Container>
+      <Text>Home</Text>
+    </Container>
+  );
 }
